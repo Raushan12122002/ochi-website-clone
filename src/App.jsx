6e1408dc105227b5cc744e0511e17bb73974fdc5
Raushan kumar <kraushan70550@gmail.com>
2024-02-07 10:42:08 +0530

    second time

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='bg-red-600'>App</div>
+    <>
+     <div className='bg-red-600'>Raushan kumar</div>
+    </>
+   
   )
 }
 
